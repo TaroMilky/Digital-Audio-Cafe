efae63d9af7c3e635a5fbed6c59453baf5767f8e
laure <laurenceipo49@gmail.com>
2026-03-29 14:36:59 -0400

    Integrated SdFat library, created PSRAM stream buffer, pinned task to core, audioTaskTrampoline

--- ESP32-S3_Backend/include/AudioPlayer.h
+++ ESP32-S3_Backend/include/AudioPlayer.h
@@ -1,13 +1,31 @@
 #ifndef AUDIO_PLAYER_H
 #define AUDIO_PLAYER_H
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/stream_buffer.h>
 #include <driver/i2s.h>
+#include <SPI.h>
+#include "SdFat.h"
 
 class AudioPlayer {
 
 private:
-    static const int BCK_PIN = 48;
-    static const int DIN_PIN = 47;
-    static const int LCK_PIN = 21;
+    StreamBufferHandle_t audioBuffer;
+    uint8_t* bufferStorage;                             // Pointer to the audio byte array in PSRAM
+    StaticStreamBuffer_t* bufferStruct;                 // Pointer to the FreeRTOS management struct in PSRAM
+    static const size_t AUDIO_BUFFER_SIZE = 128 * 1024; // 128KB buffer
+
+    SdFs sd; // SD object
+    // FsFile audioFile;
+    SPIClass spiBus{FSPI};
+    static const int SD_CS = 18;
+    static const int SD_MOSI = 17;
+    static const int SD_SCK = 16;
+    static const int SD_MISO = 15;
+
+    static const int BCK_PIN = 12;
+    static const int DIN_PIN = 13;
+    static const int LCK_PIN = 14;
     i2s_port_t i2s_port = I2S_NUM_0;
 
     float current_volume = 0.0f;
@@ -16,6 +34,10 @@ private:
 
 public:
     void init();
+    bool initSDCard();
+    static void audioTaskTrampoline(void* _thisInstance);
+    void audioTask();
+    bool openAudioFile(const char* filename);
     void setVolume(float target_volume_from_main);
     void setSmoothingFactor(float smoothing_factor_from_main);
     void playSineWave();
--- ESP32-S3_Backend/src/AudioPlayer.cpp
+++ ESP32-S3_Backend/src/AudioPlayer.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
+#include <esp_heap_caps.h> // PSRAM memory functions
 #include <math.h>
 #include "AudioPlayer.h"
 
-void AudioPlayer::init() { // CRANK UP QUALITY UPON COMPLETION OF SD CARD READING LOGIC
+void AudioPlayer::init() {
+    
+    bufferStorage = (uint8_t*)heap_caps_malloc(AUDIO_BUFFER_SIZE, MALLOC_CAP_SPIRAM);
+    bufferStruct = (StaticStreamBuffer_t*)heap_caps_malloc(sizeof(StaticStreamBuffer_t), MALLOC_CAP_SPIRAM);
+
+    if (bufferStorage == NULL || bufferStruct == NULL) {
+        Serial.println("FATAL: Failed to allocate PSRAM for Audio Buffer!");
+        return;
+    }
+
+    audioBuffer = xStreamBufferCreateStatic(AUDIO_BUFFER_SIZE, 1024, bufferStorage, bufferStruct);
+    Serial.println("128KB PSRAM Audio Buffer Initialized!");
+
+    // CRANK UP QUALITY UPON COMPLETION OF SD CARD READING LOGIC    
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX), // ESP32 is the boss (Master), Transmitting data (TX)
         .sample_rate = 44100,                                // Standard CD audio sample rate
@@ -24,7 +38,43 @@ void AudioPlayer::init() { // CRANK UP QUALITY UPON COMPLETION OF SD CARD READIN
     };
     i2s_driver_install(i2s_port, &i2s_config, 0, NULL);
     i2s_set_pin(i2s_port, &pin_config);
+
+    xTaskCreatePinnedToCore(      
+        AudioPlayer::audioTaskTrampoline,      // 1. Pointer to the task function
+        "AudioTask",                           // 2. Name of task (for debugging, e.g., "AudioTask")
+        8192,                                  // 3. Stack size in words (Use 4096 or 8192 for now)
+        this,                                  // 4. Parameter to pass to function (Usually NULL)
+        2,                                     // 5. Task priority (0 is lowest, 1 is normal, configMAX_PRIORITIES-1 is highest)
+        NULL,                                  // 6. Task handle (NULL if you don't need to kill it later)
+        1                                      // 7. Core ID (0 or 1)
+    );
 }
+
+bool AudioPlayer::initSDCard() {
+    spiBus.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+
+    // Next: Send UART to STM32, SD Card initialized/not
+    if(!sd.begin(SdSpiConfig(SD_CS, DEDICATED_SPI, SD_SCK_MHZ(16), &spiBus))) {
+        Serial.println("SD Card Mount Failed!");
+        return false;
+    }
+    Serial.printf("SD Card Mounted! FAT Type: FAT%d\n", sd.fatType());
+    return true;
+}
+
+void AudioPlayer::audioTaskTrampoline(void* _thisInstance) {
+    AudioPlayer* player = static_cast<AudioPlayer*>(_thisInstance);
+    player->audioTask();
+}
+
+void AudioPlayer::audioTask() {
+    while(1) {
+        // Stream Buffer reading logic here
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+// space for file system --------------------------------------------------------------
 
 void AudioPlayer::setVolume(float volume_from_main) { // Volume setter and perceptual volume scaler
     target_volume = (volume_from_main * volume_from_main * volume_from_main);
